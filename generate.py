75273eb77220af903962a8dec6b00131d9222458
Andrei Rybak <rybak.a.v@gmail.com>
2022-08-28 23:19:53 +0200

    extract function circle(), tweak sizes

--- generate.py
+++ generate.py
@@ -5,21 +5,24 @@ codes = []
 for c in 'Hello, world!':
     codes.append('{0:08b}'.format(ord(c)))
 
-for c in codes:
-    print(c)
-
 radius = 8
-gap = 2
+gap = 5
+stroke_width = 3
+
+
+def circle(x, y):
+    print("""<ellipse
+       style="fill:none;stroke:#fffffe;stroke-width:{};stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+       id="path817"
+       cx="{}"
+       cy="{}"
+       rx="{}"
+       ry="{}"
+       />""".format(stroke_width, x, y, radius, radius))
+
 
 for i, c in enumerate(codes):
     for j, d in enumerate(c):
         x = gap + radius + j * (gap + 2 * radius)
         y = gap + radius + i * (gap + 2 * radius)
-        print("""<ellipse
-           style="fill:none;stroke:#fffffe;stroke-width:4;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
-           id="path817"
-           cx="{}"
-           cy="{}"
-           rx="{}"
-           ry="{}"
-           />""".format(x, y, radius, radius))
+        circle(x, y)
